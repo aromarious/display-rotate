9978958b4368f6258eae9d8ab427d2fd3d1d9ac3
aromarious <6535448+aromarious@users.noreply.github.com>
2024-12-04 18:39:50 +0900

    [feat] github actions added

--- Formula/display-rotate.rb
+++ Formula/display-rotate.rb
@@ -2,18 +2,18 @@ class DisplayRotate < Formula
   desc "A command-line tool to Rotate your display on macOS using displayplacer"
   homepage "https://github.com/aromarious/display-rotate"
   url "https://github.com/aromarious/display-rotate/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed" # You'll need to fill this after creating the release
+  sha256 "81108d0cc50886b5bf121697cd49fed4c727c1f98adb5eaab777828560db8ea8"
   license "ISC"
 
   depends_on "node"
-  depends_on "typescript"
+  depends_on "displayplacer"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    system "npm", "run", "build"
+    # Install only necessary files to libexec
+    libexec.install Dir["dist", "bin", "package.json"]
 
     # Create the binary link
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    (bin/"display-rotate").write_env_script libexec/"bin/display-rotate", PATH: "#{Formula["node"].opt_bin}:$PATH"
   end
 
   test do
