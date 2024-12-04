7bf4e41faa32aa6838ca2965825ad59d77918dd7
aromarious <6535448+aromarious@users.noreply.github.com>
2024-12-04 17:17:10 +0900

    [fix] add SHA256 hash

--- Formula/display-rotate.rb
+++ Formula/display-rotate.rb
@@ -2,7 +2,7 @@ class DisplayRotate < Formula
   desc "A command-line tool to Rotate your display on macOS using displayplacer"
   homepage "https://github.com/aromarious/display-rotate"
   url "https://github.com/aromarious/display-rotate/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" # You'll need to fill this after creating the release
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed" # You'll need to fill this after creating the release
   license "ISC"
 
   depends_on "node"
@@ -11,7 +11,7 @@ class DisplayRotate < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     system "npm", "run", "build"
-    
+
     # Create the binary link
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
